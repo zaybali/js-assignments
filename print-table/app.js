f23ec9012bb95c2db177bf6c4949f34e59531e3b
zaibali <zaibali51@live.com>
2024-03-08 14:50:25 +0500

    added print table assignment

---
+++ print-table/app.js
@@ -0,0 +1,14 @@
+let tableNumber = +prompt("Please enter Table Number");
+let count = 1;
+
+
+document.write(tableNumber + " x " + count + " = " + (tableNumber*count++ + "<br>"));
+document.write(tableNumber + " x " + count + " = " + (tableNumber*count++  + "<br>"));
+document.write(tableNumber + " x " + count + " = " + (tableNumber*count++  + "<br>"));
+document.write(tableNumber + " x " + count + " = " + (tableNumber*count++  + "<br>"));
+document.write(tableNumber + " x " + count + " = " + (tableNumber*count++  + "<br>"));
+document.write(tableNumber + " x " + count + " = " + (tableNumber*count++  + "<br>"));
+document.write(tableNumber + " x " + count + " = " + (tableNumber*count++  + "<br>"));
+document.write(tableNumber + " x " + count + " = " + (tableNumber*count++  + "<br>"));
+document.write(tableNumber + " x " + count + " = " + (tableNumber*count++  + "<br>"));
+document.write(tableNumber + " x " + count + " = " + (tableNumber*count++  + "<br>"));
